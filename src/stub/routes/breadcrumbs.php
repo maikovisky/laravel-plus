63c015e4a3021bef44eb4423efbe6198f47c5336
Maiko de Andrade <maiko.andrade@senairs.org.br>
2017-02-09 11:26:38 -0200

    Add fix resource, request, add breadcrumps

---
+++ src/stub/routes/breadcrumbs.php
@@ -0,0 +1,19 @@
+
+// Home > {{snake_name}}
+Breadcrumbs::register('type_attend', function($breadcrumbs)
+{
+    $breadcrumbs->parent('home');
+    $breadcrumbs->push(trans('general.{{Name}}'), route('{{snake_name}}.index'));
+});
+
+// Home > {{snake_name}} > Edit
+Breadcrumbs::register('{{snake_name}}.edit', function($breadcrumbs, ${{camel_name}} = null)
+{
+    $breadcrumbs->parent('{{snake_name}}');
+    if(isset(${{camel_name}}->id)) {
+        $breadcrumbs->push(${{camel_name}}->name, route('{{snake_name}}.update', ${{camel_name}}->id));
+    }
+    else {
+        $breadcrumbs->push(Lang::get("general.New"), route('{{snake_name}}.create'));
+    }  
+});
